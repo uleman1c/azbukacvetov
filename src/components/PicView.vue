b5d9c1c99ed52c5bcb1001e766f3a21fd7b0a6ee
Mihail <uleman@mail.ru>
2023-07-10 07:29:34 +0300

    PicView

---
+++ src/components/PicView.vue
@@ -0,0 +1,16 @@
+<template>
+    <img :src="url" v-bind:alt="pic">
+</template>
+
+<script>
+
+export default {
+
+    props: {
+        url: String
+    }
+
+}
+
+</script>
+
--- src/views/ItemView.vue
+++ src/views/ItemView.vue
@@ -1,44 +1,43 @@
 <template>
-    <h5>Item {{ item }}</h5>
-    <img :src="item.pics[0]" v-bind:alt="pic">
+    <h1>{{ item.name }}</h1>
+
+    <h2>Цена: {{ item.price.toLocaleString('ru', { minimumFractionDigits: 2 }) }}</h2>
+    <h2>Диаметр: {{ item.diameter }}, высота: {{ item.height }}</h2>
+
+    <PicView v-for="pic in item.pics"
+        v-bind:url="pic"
+    />
+
 </template>
 
 <script>
 
 import tables from '@/tables.js'
+import PicView from '../components/PicView.vue'
 
 export default{
-
-    data(){
-
+    data() {
         return {
-
             item: {
-                id: '',
-                name:'',
+                id: "",
+                name: "",
                 price: 0,
                 diameter: 0,
                 height: 0,
-                pics: [] 
+                pics: []
             }
-
-        }
-
+        };
     },
-
-    mounted(){
-
-        this.item.id = this.$route.query.id
-
-        const curItem = tables.items.filter(i => i.id == this.item.id)[0]
-
-        this.item.name = curItem.name
-        this.item.price = curItem.price
-        this.item.diameter = curItem.diameter
-        this.item.height = curItem.height
-        this.item.pics = curItem.pics
-    }
-
+    mounted() {
+        this.item.id = this.$route.query.id;
+        const curItem = tables.items.filter(i => i.id == this.item.id)[0];
+        this.item.name = curItem.name;
+        this.item.price = curItem.price;
+        this.item.diameter = curItem.diameter;
+        this.item.height = curItem.height;
+        this.item.pics = curItem.pics;
+    },
+    components: { PicView }
 }
 
 </script>
