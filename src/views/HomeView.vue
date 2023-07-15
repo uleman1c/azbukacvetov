de0fdd59b3a097d7676b3bb61a5aa20b07935b7e
Mihail <uleman@mail.ru>
2023-07-15 06:35:32 +0300

    adapt 1

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,6 +5,38 @@
       style="z-index: 0; position: absolute; left: 0; top: 0; background-repeat: no-repeat;width: 100%;height: 100%;background-size: cover;">
     </div>
 
+    <div style="position: relative">
+      <div class="adaptive">
+
+        <div class="header">
+
+        </div>
+
+        <div class="menu">
+
+        </div>
+
+        <div class="items">
+
+          <div v-for="item in items" class="item">
+            <div
+              class="item__img"
+              v-bind:style="'--img: url(' + (item.pics && item.pics.length > 0 ? item.pics[0] : '') + ')'"
+
+            ></div>
+            <div class="item__content">
+              <div class="item__title">{{ item.name }}</div>
+              <div class="item__desc">{{ item.price ? item.price.toLocaleString('ru', { minimumFractionDigits: 2 }) : '' }}</div>
+            </div>
+          </div>
+          
+
+        </div>
+
+      </div>
+
+    </div>
+
     <h2 style="position: relative; text-align: center; margin: 0;">Контакты +79052700065, +79817338935</h2>
     <h2 style="position: relative; text-align: center; margin: 0;">Электронная почта anastasiy.azbuka@icloud.com</h2>
     <h2 style="position: relative; text-align: center; margin: 0;">Адрес Пулковское шоссе, 120 Санкт-Петербург</h2>
@@ -114,6 +146,61 @@ export default {
 </script>
 
 <style scoped>
+
+.adaptive {
+  width: 100%;
+  height: 100%;
+  margin: auto;
+  display: grid;
+  grid-template-columns: 1fr 350px;
+  grid-template-rows: 100px 1fr;
+  grid-auto-rows: 50px;
+  gap: 10px;
+}
+
+.header {
+  display: flex;
+  gap: 15px;
+  align-items: center;
+  justify-content: space-between;
+  font-size: 14px;
+  grid-column: span 2;
+}
+
+.items {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(400px, auto));
+  gap: 10px;
+}
+.item {
+  display: grid;
+  background: var(--primary);
+  color: var(--invert_primary);
+  border-radius: var(--radius);
+  overflow: hidden;
+  padding: 5px 5px 15px;
+}
+
+.item__img {
+  height: 240px;
+  margin-bottom: 10px;
+  border-radius: var(--radius);
+  background: #333 var(--img) center / cover;
+}
+.item__content {
+  padding: 0 10px;
+  text-align: left;
+}
+.item__title {
+  font-size: 24px;
+  font-weight: 500;
+  margin-bottom: 5px;
+  color: var(--invert_primary);
+}
+.item__desc {
+  font-weight: 300;
+}
+
 
 
 </style>
